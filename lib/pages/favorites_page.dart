ec625a7e17c879055e399e517893418a71f8cd5d
Bruna Lusa Balbinot <brunalusabalbinot@MacBook-Pro-de-Bruna.local>
2020-11-12 21:40:14 -0300

    quando clicar numa receita favorita, abrir a pagina de edição de receitas

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:minhasreceitas/controllers/favorites_controller.dart';
+import 'package:minhasreceitas/model/recipe.dart';
+import 'package:minhasreceitas/pages/new_recipe_page.dart';
 
 class Favorites extends StatefulWidget {
   static const String title = 'Receitas Favoritas';
@@ -23,56 +25,66 @@ class _FavoritesState extends State<Favorites> {
     super.initState();
   }
 
+  _openEditRecipe(Recipe recipe) {
+    Navigator.of(context, rootNavigator: true)
+        .pushNamed(NewRecipe.routeName, arguments: recipe);
+  }
+
   Widget _listBuilder(BuildContext context, int index) {
     return SafeArea(
         top: false,
         bottom: false,
-        child: Slidable(
-          actionPane: SlidableDrawerActionPane(),
-          actionExtentRatio: 0.25,
-          child: Card(
-            elevation: 1.5,
-            margin: EdgeInsets.fromLTRB(6, 0, 6, 0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    PlatformIcons(context).heartSolid,
-                    color: Color(0xFFB22222),
-                  ),
-                  Padding(padding: EdgeInsets.only(left: 16)),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          controller.recipes[index].name,
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Padding(padding: EdgeInsets.only(top: 8)),
-                      ],
+        child: GestureDetector(
+          onTap: () {
+            _openEditRecipe(controller.recipes[index]);
+          },
+          child: Slidable(
+            actionPane: SlidableDrawerActionPane(),
+            actionExtentRatio: 0.25,
+            child: Card(
+              elevation: 1.5,
+              margin: EdgeInsets.fromLTRB(6, 0, 6, 0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      PlatformIcons(context).heartSolid,
+                      color: Color(0xFFB22222),
                     ),
-                  ),
-                ],
+                    Padding(padding: EdgeInsets.only(left: 16)),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            controller.recipes[index].name,
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 8)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+            actions: <Widget>[
+              IconSlideAction(
+                  caption: 'Desfavoritar',
+                  color: Colors.red,
+                  icon: PlatformIcons(context).favoriteSolid,
+                  onTap: () => controller.disfavor(index))
+            ],
           ),
-          actions: <Widget>[
-            IconSlideAction(
-                caption: 'Desfavoritar',
-                color: Colors.red,
-                icon: PlatformIcons(context).favoriteSolid,
-                onTap: () => controller.disfavor(index))
-          ],
         ));
   }
 
